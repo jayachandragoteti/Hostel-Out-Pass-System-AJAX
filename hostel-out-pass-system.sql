e030ca7497620e932783bd67eb40254e6287fae2
GOTETI JAYACHANDRA MOHAN LAKSHMI NARASIMHA MURTHY <63448352+jayachandragoteti@users.noreply.github.com>
2021-03-04 15:59:38 +0530

    block images are added

---
+++ hostel-out-pass-system.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 04, 2021 at 05:55 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hostel-out-pass-system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blocks`
+--
+-- Error reading structure for table hostel-out-pass-system.blocks: #1932 - Table 'hostel-out-pass-system.blocks' doesn't exist in engine
+-- Error reading data for table hostel-out-pass-system.blocks: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hostel-out-pass-system`.`blocks`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `perpermissions_details`
+--
+-- Error reading structure for table hostel-out-pass-system.perpermissions_details: #1932 - Table 'hostel-out-pass-system.perpermissions_details' doesn't exist in engine
+-- Error reading data for table hostel-out-pass-system.perpermissions_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hostel-out-pass-system`.`perpermissions_details`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+-- Error reading structure for table hostel-out-pass-system.rooms: #1932 - Table 'hostel-out-pass-system.rooms' doesn't exist in engine
+-- Error reading data for table hostel-out-pass-system.rooms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hostel-out-pass-system`.`rooms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site`
+--
+-- Error reading structure for table hostel-out-pass-system.site: #1932 - Table 'hostel-out-pass-system.site' doesn't exist in engine
+-- Error reading data for table hostel-out-pass-system.site: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hostel-out-pass-system`.`site`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student_details`
+--
+-- Error reading structure for table hostel-out-pass-system.student_details: #1932 - Table 'hostel-out-pass-system.student_details' doesn't exist in engine
+-- Error reading data for table hostel-out-pass-system.student_details: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hostel-out-pass-system`.`student_details`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table hostel-out-pass-system.users: #1932 - Table 'hostel-out-pass-system.users' doesn't exist in engine
+-- Error reading data for table hostel-out-pass-system.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `hostel-out-pass-system`.`users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
